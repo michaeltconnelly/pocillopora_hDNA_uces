fd569f8f000170b1be989d783ace3c465a1d3570
michaeltconnelly <mconnelly@rsmas.miami.edu>
2023-12-11 17:09:33 -0500

    add scripts for partitions, iqtree

---
+++ bash/sed_partitions.sh
@@ -0,0 +1,2 @@
+infile=$(basename *.charsets)
+sed 's/charset/DNA,/g' ${infile}.charsets | sed 's/;//g' | sed 's/ = /=/g' | sed "s/'*'//g" | sed '1d; 2d; $d' | grep -v "charpartition" > ${infile}.partitions
