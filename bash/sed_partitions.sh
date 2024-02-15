d6f27b2c76d419e70328cb9e99d9657010e13a99
michaeltconnelly <mconnelly@rsmas.miami.edu>
2024-02-15 09:23:28 -0500

    modify partitions and iqtree jobs

--- bash/sed_partitions.sh
+++ bash/sed_partitions.sh
@@ -1,2 +1,2 @@
-infile=$(basename *.charsets)
+infile=$(basename *.charsets | cut -d . -f 1)
 sed 's/charset/DNA,/g' ${infile}.charsets | sed 's/;//g' | sed 's/ = /=/g' | sed "s/'*'//g" | sed '1d; 2d; $d' | grep -v "charpartition" > ${infile}.partitions
