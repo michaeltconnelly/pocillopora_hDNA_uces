504de5b7d5e4331a3a62cb7cf5295767c5d84ab4
michaeltconnelly <mconnelly@rsmas.miami.edu>
2024-03-11 16:06:34 -0400

    add fastqdump wrapper to get Nico's SRA files for best GSH samples

--- bash/fastqdump_wrapper.sh
+++ bash/fastqdump_wrapper.sh
@@ -1,39 +1,35 @@
 #!/bin/bash
 
-prodir="/scratch/nmnh_corals/connellym/projects/MetaPocillopora"
-study="$1"
-samples=$(cat ${prodir}/data/SRR_Acc_List_${study}.txt)
+prodir="/scratch/nmnh_corals/connellym/projects/pocillopora_hDNA_uces"
+samples=$(cat ${prodir}/data/oury_top_SRA_accessions.txt)
+
+JOBFILE=${prodir}/bash/jobs/fastqdump_${sample}.job
 
 for sample in $samples
 do
-echo "# /bin/sh" > $prodir/bash/jobs/fastqdump_${study}_${sample}.job
+echo "# /bin/sh" > $JOBFILE
 #
-echo "# ----------------Parameters---------------------- #" >> $prodir/bash/jobs/fastqdump_${study}_${sample}.job
 echo "#$  -S /bin/sh
 #$ -pe mthread 4
 #$ -q sThC.q
 #$ -l mres=4G,h_data=4G,h_vmem=4G
 #$ -cwd
 #$ -j y
-#$ -N fastqdump_${study}_${sample}
-#$ -o $prodir/bash/jobs/fastqdump_${study}_${sample}
+#$ -N fastqdump_${sample}
+#$ -o $prodir/bash/jobs/fastqdump_${sample}
 #$ -m bea
-#$ -M connellym@si.edu" >> $prodir/bash/jobs/fastqdump_${study}_${sample}.job
+#$ -M connellym@si.edu
+#" >> $JOBFILE
 #
-echo "# ----------------Modules------------------------- #" >> $prodir/bash/jobs/fastqdump_${study}_${sample}.job
-echo "module load bioinformatics/sratoolkit" >> $prodir/bash/jobs/fastqdump_${study}_${sample}.job
-echo "#" >> $prodir/bash/jobs/fastqdump_${study}_${sample}.job
-echo "# ----------------Your Commands------------------- #" >> $prodir/bash/jobs/fastqdump_${study}_${sample}.job
+
+echo "module load bioinformatics/sratoolkit" >> $JOBFILE
 #
-echo 'echo + `date` job $JOB_NAME started in $QUEUE with jobID=$JOB_ID on $HOSTNAME' >> $prodir/bash/jobs/fastqdump_${study}_${sample}.job
-echo 'echo + NSLOTS = $NSLOTS' >> $prodir/bash/jobs/fastqdump_${study}_${sample}.job
+echo 'echo + `date` job $JOB_NAME started in $QUEUE with jobID=$JOB_ID on $HOSTNAME' >> $JOBFILE
+echo 'echo + NSLOTS = $NSLOTS' >> $JOBFILE
 #
-echo "mkdir ${prodir}/data/srareads/${study}" >> $prodir/bash/jobs/fastqdump_${study}_${sample}.job
-echo "#" >> $prodir/bash/jobs/fastqdump_${study}_${sample}.job
+echo "fastq-dump --split-files --outdir ${prodir}/data/oury_sra/${study} --gzip $sample" >> $JOBFILE
 #
-echo "fastq-dump --split-files --outdir ${prodir}/data/srareads/${study} --gzip $sample" >> $prodir/bash/jobs/fastqdump_${study}_${sample}.job
-#
-echo "#" >> $prodir/bash/jobs/fastqdump_${study}_${sample}.job
-echo 'echo = `date` job $JOB_NAME done' >> $prodir/bash/jobs/fastqdump_${study}_${sample}.job
-qsub $prodir/bash/jobs/fastqdump_${study}_${sample}.job
+echo "#" >> $JOBFILE
+echo 'echo = `date` job $JOB_NAME done' >> $JOBFILE
+qsub $JOBFILE
 done
