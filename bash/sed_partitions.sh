1a0908d73a215cf377342ce9952415b359aebd68
michaeltconnelly <mconnelly@rsmas.miami.edu>
2024-03-13 09:38:53 -0400

    adjust phyluce final scripts to produce 75% and 50% data matrices, partition files

--- bash/sed_partitions.sh
+++ bash/sed_partitions.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+cd $1
 infile=$(basename *.charsets | cut -d . -f 1)
 sed 's/charset/DNA,/g' ${infile}.charsets | sed 's/;//g' | sed 's/ = /=/g' | sed "s/'*'//g" | sed '1d; 2d; $d' | grep -v "charpartition" > ${infile}.partitions
